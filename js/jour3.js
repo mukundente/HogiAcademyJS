4cf7be22ab87f9472bf9ac16282d23c6f0c8c9c1
nukundente <mukundentepeace@gmail.com>
2022-08-05 17:53:30 +0200

    clean up

---
+++ js/jour3.js
@@ -0,0 +1,126 @@
+let age =14;
+if(age>=18){
+    console.log('vous pouvez entre')
+}
+else{
+    console.log('sorte,vous etes encore minieur')
+}
+
+
+var x=20;
+if(x  %2 ==1)
+{
+    console.log ('le nombre est en pair')
+
+}
+else{
+    console/log('le nombre est en impair')
+}
+
+let x 17;
+if(x/x && x/1) {
+    console.log('le nombre est premier')
+} else{
+    console.log('le nombre n est pas le premier')
+}
+
+
+let b=true
+let resultat =b &&(1/0)
+console.log(resultat)
+
+let b2= false
+let result2 =b2 &&(1/0)
+    console.log(result2)
+
+let y =null
+if(y){
+    console.log('la valeur de y existe')
+}else{
+    console.log('le valeur de y n exite pas')
+}
+let a =10;
+let b =20;
+if (a>b ){
+    console.log('le max est a')
+    }else{
+        console.log('le max est b')
+    }
+let a =10;
+let b=20;
+let c=30
+if(a>b){
+    if(b/c)
+     console.log('a est max');
+}   
+//1.determiner le mmax entre let a=19; let b=20; let c=30
+
+
+let a = prompt("donner la valeur de a");
+let b = prompt("donner la valeur de b");
+let c = prompt("donner la valeur c")
+
+if(a>b && b < c) {
+    console.log("a est max")
+    if (b>c && c < a){
+        console.log("b est max");
+    
+}else{
+    if(c>a && a<b){
+        console.log("c est max")
+    }
+}
+}
+
+//2.ercire ("hello hogi")50 fois
+
+
+let a =0;
+while(a<50)
+{
+    console.log("hello hogi,:"+a++);
+}
+
+//3 equation ax**2bx+c=0
+
+
+let a = prompt("donner la valeur de a");
+let b = prompt("donner la valeur de b");
+let c = prompt("donner la valeur c")
+let delta = b*b-4(a*c);
+if(delta>0){
+    y=math.aqrt(delta);
+    let x1= (-b+y)/2;
+    let x2= (-b-y)/2;
+    console.log ("x1 est:"+x1);
+    console.log("x2 est:"+x2);}
+
+    else{
+        if (delta==0){
+        let v=-b/2
+        console.log("x1 et x2 est:"+v);
+            
+    }
+        else{console.log("pas de reponse car delta ne peut psa etre negatif")
+        
+    }
+}
+
+      //1tableaux java script
+      // la solution  du tableaux
+      //pourqoui le tableaux
+      //creation d'un tableaux
+
+      let x = 4
+       x = x+2
+      console.log(x)
+      >6
+       x+=4
+       console.log(x)
+     >10
+
+
+     
+
+
+     
